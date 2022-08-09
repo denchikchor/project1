1979e25725ed887c6ebac3a6a20e3531d5a6779c
Denys <example@gmail.com>
2022-08-09 11:25:51 +0300

    secind commit

--- new.js
+++ new.js
@@ -1,39 +1,19 @@
 "use strict";
 
-if (3 == 9) {
-    console.log('ok');
-} else {
-    console.log('error');
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// } 
+
+
+do {
+    console.log(num);
+    num++;
 }
 
+while (num < 55);
 
 
-// if ( num < 49 ) {
-//     console.log('errror');
-// } else if(num > 100) {
-//     console.log('too musc');
-// } else {
-//     console.log('Ok!');
-// }
-
-// (num === 50) ? console.log('Ok!') : console.log('errror');
-
-
-const num = 50;
-
-
-switch (num) {
-    case 49:
-        console.log('FalsE');
-        break;
-    case 100:
-        console.log('FalsE');
-        break;
-    case 50:
-        console.log('TrUe');
-        break;
-    default:
-        console.log('Mb TrUe');
-        break;
-
-}
+for (let i = 1; let)
